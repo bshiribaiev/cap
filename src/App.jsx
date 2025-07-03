a686fac2c0fdba58a58239e759d4546b114615b6
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-07-02 21:08:14 -0400

    added api query

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import APIform from './components/APIform'
 
 function App() {
   const ACCESS_KEY = import.meta.env.VITE_API_FLASH_KEY
+  const [currentImage, setCurrentImage] = useState(null);
   const [inputs, setInputs] = useState({
     url: "",
     format: "",
@@ -21,13 +22,62 @@ function App() {
       width: "1920",
       height: "1080",
     };
+    if (inputs.url === "" || inputs.url === " ") {
+    alert("Please put valid url")
+    }
+    else {
+      const updatedInputs = { ...inputs };
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value === "") {
+          updatedInputs[key] = defaultValues[key];
+        }
+      }
+      setInputs(updatedInputs);
+      const query = makeQuery()
+      callAPI(query).catch(console.error)
+    }
   }
 
-  if (inputs.url == "" || inputs.url == " ") {
-    alert("Please put valid url")
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+    let query = `https://api.apiflash.com/v1/urltoimage?
+                access_key=${ACCESS_KEY}
+                &url=${fullURL}
+                &format=${inputs.format}
+                &width=${inputs.width}
+                &height=${inputs.height}
+                &no_cookie_banners=${inputs.no_cookie_banners}
+                &no_ads=${inputs.no_ads}
+                &wait_until=${wait_until}
+                &response_type=${response_type}
+                &fail_on_status=${fail_on_status}`;
+    return query
   }
-  else {
-    
+  
+  const callAPI = async (query) => {
+    const response = await fetch(query);
+    const json = await response.json();
+    if (json.url == null) {
+      alert("Oops! Something went wrong with that query, let's try again!")
+        }
+    else {
+      setCurrentImage(json.url);
+      reset()
+    }
+  }
+  const reset = () => {
+    setInputs({
+      url: "",
+      format: "",
+      no_ads: "",
+      no_cookie_banners: "",
+      width: "",
+      height: "",
+    });
   }
 
   return (
@@ -44,6 +94,34 @@ function App() {
         }
         onSubmit={submitForm}
         />
+        <br></br>
+        {currentImage ? (
+          <img
+            className="screenshot"
+            src={currentImage}
+            alt="Screenshot returned"
+          />
+        ) : (
+          <div> </div>
+        )}
+        <div className="container">
+          <h3> Current Query Status: </h3>
+          <p>
+            https://api.apiflash.com/v1/urltoimage?access_key=ACCESS_KEY    
+            <br></br>
+            &url={inputs.url} <br></br>
+            &format={inputs.format} <br></br>
+            &width={inputs.width}
+            <br></br>
+            &height={inputs.height}
+            <br></br>
+            &no_cookie_banners={inputs.no_cookie_banners}
+            <br></br>
+            &no_ads={inputs.no_ads}
+            <br></br>
+          </p>
+        </div>
+
         <br></br>
     </div>
   )
